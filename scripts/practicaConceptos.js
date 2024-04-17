3707689bd184e44ba639a17c88108845f5354ced
yeisonaguzman <yeisonaguzman@gmail.com>
2024-04-17 08:13:58 -0500

    termino actividad

--- scripts/practicaConceptos.js
+++ scripts/practicaConceptos.js
@@ -1,9 +1,49 @@
-// 1. Siempre se debe hacer -> verificar que si esté conectado con el HTML
+// 1. SIEMPRE SE DEBE HACER -> VERIFICAR QUE SI ESTÉ CONECTADO CON EL HTML
 
-console.log("Hola a todos, soy la actividad de hoy 10/04/2024"); 
+console.log("Holaaaa, soy la actividad de hoy");
 
-// 2. TRAER LOS ELEMENTOS QUE NECESITA DE SU HTML -> los que usualmente tienen identificador unico
 
+// 2. TRAER LOS ELEMENTOS QUE NECESITAS DE SU HTML -> los que tienen id -> los elemntos que me traiga deben estar en una variable -> debe ser constante
+
+//document-> me accede a mi html
+//getElemntById -> me trae un elemento en particular por su identificador único
+//dentro de los paréntesis DEBO PONER EL MISMO ID que puse en mi html
+
+//Mi elemento 1 -> forma 1-> se llevan directamente el elemento a su js
 const edadInput = document.getElementById("edadInput");
 console.log(edadInput);
 
+//Mi elemento 2 -> una forma
+// const btn = document.getElementById("boton");
+// console.log(btn);
+
+
+const mensaje = document.getElementById("mensajeEdad");
+console.log(mensaje);
+
+
+//2da forma es mediante eventos y crean funciones asociadas
+function verificarEdad(){
+    //Yo quiero que cada que le de click al botón, me salga un mensaje de "hola"
+
+    console.log("Entró a mi funcion verificar edad");
+
+    //para acceder no al elemento del html sino a su valor, se debe escribir el .value
+    let edad = edadInput.value;
+    console.log(edad);
+//UTILIZAR CONDICIONALES
+
+    if(edad>= 18){
+
+        //acá esto es lo que sucede si la condición es verdadera
+        console.log("Eres mayor de edad");
+
+        console.log(mensaje.textContent="Claro que si, ingresa")
+    } else{
+        //acá se define lo que sucede cuando la condición es falsa
+        console.log("Eres menor de edad");
+
+        mensaje.textContent= "Este contenido no se puede visualizar"
+    }
+
+}
